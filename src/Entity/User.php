a8ff466c65ac0ba9235a6ea9df3a88f85b3c7869
ndaen <daennoah@gmail.com>
2023-11-14 19:55:12 +0100

    feat: login, logout

--- src/Entity/User.php
+++ src/Entity/User.php
@@ -7,10 +7,12 @@ use Doctrine\Common\Collections\ArrayCollection;
 use Doctrine\Common\Collections\Collection;
 use Doctrine\DBAL\Types\Types;
 use Doctrine\ORM\Mapping as ORM;
+use Symfony\Bridge\Doctrine\Validator\Constraints\UniqueEntity;
 use Symfony\Component\Security\Core\User\PasswordAuthenticatedUserInterface;
 use Symfony\Component\Security\Core\User\UserInterface;
 
 #[ORM\Entity(repositoryClass: UserRepository::class)]
+#[UniqueEntity(fields: ['email'], message: 'There is already an account with this email')]
 class User implements UserInterface, PasswordAuthenticatedUserInterface
 {
     #[ORM\Id]
@@ -77,18 +79,19 @@ class User implements UserInterface, PasswordAuthenticatedUserInterface
      */
     public function getRoles(): array
     {
-        $roles = $this->roles;
+       /* $roles = $this->roles;
         // guarantee every user at least has ROLE_USER
         $roles[] = 'ROLE_USER';
 
-        return array_unique($roles);
+        return array_unique($roles);*/
+        return [];
     }
 
     public function setRoles(array $roles): static
     {
-        $this->roles = $roles;
+      /*  $this->roles = $roles;
 
-        return $this;
+        return $this;*/
     }
 
     /**
